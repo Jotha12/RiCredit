46e029a8910e3b6fe097557c38f0cec77883b849
Jotha12 <72186230+Jotha12@users.noreply.github.com>
2020-10-01 23:56:10 -0600

    Agregar archivos de proyecto.

---
+++ RiCredit/Controllers/PrincipalController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RiCredit.Models;
+
+namespace RiCredit.Controllers
+{
+    public class PrincipalController : Controller
+    {
+        public IActionResult Principal()
+
+            
+
+        {
+            Principal obj = new Principal();
+
+            obj.Nombre = "Jonatan";
+
+            return View(obj);
+        }
+    }
+}
