79db4e50dd3711f6a2ebfc06afbc7b23491af34d
Alexis Jimenez <Alexis Jimenez@DESKTOP-OSG763A>
2020-10-03 08:57:15 -0600

    m2

---
+++ RiCredit/Controllers/ClienteController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ricredit.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cliente(string nombre)
+        {
+            ViewBag.Nombre = nombre;
+            return View();
+
+        }
+    }
+}
