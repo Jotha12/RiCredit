819d01a15579deac1ec97a2ae8ce14c98ccdb219
Alexis Jimenez <Alexis Jimenez@DESKTOP-OSG763A>
2020-10-10 01:14:40 -0600

    Modificacion Vistas

---
+++ RiCredit/Controllers/EjVentasController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ricredit.Controllers
+{
+    public class EjVentasController : Controller
+    {
+        public IActionResult EjVentas()
+        {
+            return View();
+        }
+    }
+}
