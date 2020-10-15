aa7dd40c2aaceba7557827a8448a3d6dc4ea0fb0
Alexis Jimenez <Alexis Jimenez@DESKTOP-OSG763A>
2020-10-15 02:10:05 -0600

    Modificar vista Nueva Solicitud

---
+++ RiCredit/Controllers/NSolicitudController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ricredit.Controllers
+{
+    public class NSolicitudController : Controller
+    {
+        public IActionResult NSolicitud()
+        {
+            return View();
+        }
+    }
+}
