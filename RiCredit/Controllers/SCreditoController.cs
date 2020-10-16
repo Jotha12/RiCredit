eea112e14b926ab583f158eea309b7b89e0f926d
Alexis Jimenez <Alexis Jimenez@DESKTOP-OSG763A>
2020-10-16 01:29:17 -0600

    modificar vista credito

---
+++ RiCredit/Controllers/SCreditoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ricredit.Controllers
+{
+    public class SCreditoController : Controller
+    {
+        public IActionResult SCredito()
+        {
+            return View();
+        }
+    }
+}
---
+++ RiCredit/Models/SCredito.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ricredit.Models
+{
+    public class SCredito
+    {
+        public string Primer_nombre { get; set; }
+        public string Segundo_nombre { get; set; }
+        public string Primer_apellido { get; set; }
+        public string Segundo_apellido { get; set; }
+        public string Dpi { get; set; }
+        public string Fecha_nacimiento { get; set; }
+        public string Telefono { get; set; }
+        public string Correo_electronico { get; set; }
+        public string Cantidad_credito { get; set; }
+        public string Cuotas { get; set; }
+        public string Ubicacion{ get; set; }
+        public string Ingresos { get; set; }
+    }
+}
